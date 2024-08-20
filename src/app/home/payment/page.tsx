5a2e46dd43ccdda73abef85990ae14b4a0179cdb
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-08-20 13:17:08 -0600

    almost final version

--- src/app/home/payment/page.tsx
+++ src/app/home/payment/page.tsx
@@ -1,5 +1,76 @@
-export default function Page(){
-    return <>
-    <p>Payment Page</p>
-    </>
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Payments from "../../img/payments.png";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col justify-center items-center max-w-[430px]">
+      <div className="flex flex-col justify-center items-center w-[90vw] max-w-[387px] h-[12vh] min-h-[90px] bg-color5 rounded-md">
+        <h1>Payment Methods</h1>
+        <Image src={Payments} width={870} height={100} alt="Payments icons" />
+      </div>
+      <div className="flex flex-col items-center bg-color5 w-[90vw] max-w-[387px] my-[2vh] rounded-md h-[40vh] min-h-[300px]">
+        <div className="text-xl my-3 font-bold">Choose Tickets</div>
+        <div className="flex">
+          <div className="flex flex-col">
+            <input
+              type="number"
+              className="m-3 w-10 text-center rounded-md"
+              defaultValue={1}
+            ></input>
+            <input
+              type="number"
+              className="m-3 w-10 text-center rounded-md"
+              defaultValue={0}
+            ></input>
+            <input
+              type="number"
+              className="m-3 w-10 text-center rounded-md"
+              defaultValue={0}
+            ></input>
+            <input
+              type="number"
+              className="m-3 w-10 text-center rounded-md"
+              defaultValue={0}
+            ></input>
+          </div>
+          <div className="flex flex-col">
+            <p className="my-3 w-32">Adult pass day</p>
+            <p className="my-3 w-32">Youth pass day</p>
+            <p className="my-3 w-32">Adult pass mo.</p>
+            <p className="my-3 w-32">Youth pass mo.</p>
+            <p className="my-3 w-32 font-bold">Total</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="my-3 w-16">$ 5.50</p>
+            <p className="my-3 w-16">$ 3.50</p>
+            <p className="my-3 w-16">$ 45.50</p>
+            <p className="my-3 w-16">$ 15.50</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="my-3 w-16 text-right">$ 5.50</p>
+            <p className="my-3 w-16 text-right">$ 0.00</p>
+            <p className="my-3 w-16 text-right">$ 0.00</p>
+            <p className="my-3 w-16 text-right">$ 0.00</p>
+            <p className="my-3 w-16 text-right font-bold">$ 5.50</p>
+          </div>
+        </div>
+        {/* <div className="flex items-center justify-between">
+          <input
+            type="number"
+            className="m-3 w-10 text-center rounded-md"
+          ></input>
+          <span> Adult Pass</span>
+          <span> x $5.50</span>
+          <span> = $11.00</span>
+        </div>
+        <input
+          type="number"
+          className="m-3 w-10 text-center  rounded-md"
+        ></input>
+        <span> Youth Pass</span> */}
+      </div>
+      <button className="w-[90vw] bg-color1 font-bold text-color5 text-xl max-w-[387px] rounded-md my-[2vh] p-[2vh]"> Proceed Checkout</button>
+    </div>
+  );
 }
--- src/app/home/trip/page.tsx
+++ src/app/home/trip/page.tsx
@@ -1,5 +1,34 @@
-export default function Page(){
-    return <>
-    <p>Trip Page</p>
-    </>
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarked, faSearch } from "@fortawesome/free-solid-svg-icons";
+
+export default function Page() {
+  return (
+    <div className="flex items-center flex-col">
+      <div className="flex items-center justify-between bg-color5 w-[90vw] max-w-[387px] my-[2vh] h-[8vh] rounded-md">
+        <FontAwesomeIcon
+          className="px-[8px] text-color1"
+          icon={faMapMarked}
+        ></FontAwesomeIcon>
+        <span> Where to</span>
+        <FontAwesomeIcon
+          className="px-[8px] text-color1"
+          icon={faSearch}
+        ></FontAwesomeIcon>
+      </div>
+      <div className="flex w-[90vw] max-w-[387px] h-[6vh] bg-color5 rounded-t-md overflow-hidden">
+        <div className="flex-1 text-center">Leave at</div>
+        <div className="flex-1 bg-color1 text-center">Arrive by</div>
+      </div>
+      <div className="flex flex-col items-start bg-color5 w-[90vw] max-w-[387px] mb-[2vh] rounded-b-md">
+        <div className="m-[8px]">
+          <input className="mx-[8px]" type="radio" name="part" defaultChecked={true} />
+          <span className="mx-[8px]"> Now </span>
+        </div>
+        <div className="m-[8px]">
+          <input className="mx-[8px]" type="radio" name="part" />
+          <input className="mx-[8px]" type="time" defaultValue="13:45:25" />
+        </div>
+      </div>
+    </div>
+  );
 }
